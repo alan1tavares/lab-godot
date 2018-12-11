edf7fe1c09bcd2671ff46292e656ad0a1e4b7189
alan1tavares <alan1tavares@gmail.com>
2018-12-11 01:55:41 -0300

    Terminado a ultima parte do tutorial. Só falta da uma arruma no HUD

--- Tutoriais da documentação/Dodge the Creeps!/Main.gd
+++ Tutoriais da documentação/Dodge the Creeps!/Main.gd
@@ -19,6 +19,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$GameOver.play()
 	pass # replace with function body
 
 func new_game():
@@ -28,6 +30,7 @@ func new_game():
 	
 	$HUD.update_score(score)
 	$HUD.show_message("Prepare-se")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
