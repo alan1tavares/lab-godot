118db651e42a29deeba54b313b2a2a97ab195b52
alan1tavares <alan1tavares@gmail.com>
2018-12-13 00:00:46 -0300

    Fazendo testes com sinais

---
+++ Tutoriais da documentação/Signal/TimerExample.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	$Timer.start()
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Timer_timeout():
+	
+	$Sprite.visible = !$Sprite.visible
+	pass # replace with function body
