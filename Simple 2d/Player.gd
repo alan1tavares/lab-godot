2d6202406b04ab1fadfd0e7c49721535f8ef38a9
alan1tavares <alan1tavares@gmail.com>
2018-12-14 12:18:39 -0300

    Tentei usar a mesma lógica de pulo do jogo que vem como exemplo, mas não deu muito certo. Provavelmente eu entendi errado o script do jogo de exemplo

--- Simple 2d/Player.gd
+++ Simple 2d/Player.gd
@@ -33,11 +33,13 @@ func _physics_process(delta):
 		motion.x = 0
 		anim = "idle"
 		
-	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			motion.y = JUMP_HEIGHT
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		motion.y = JUMP_HEIGHT
 	else:
-		anim = "jump"
+		if motion.y < 0:
+			anim = "jump"
+		else:
+			anim = "falling"
 	
 	$AnimatedSprite.play(anim)
 	motion = move_and_slide(motion, UP)
