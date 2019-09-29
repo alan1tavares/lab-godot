c1c98d91b6b8a34e6a21b09d66589f5db3585403
alan1tavares <alan1tavares@gmail.com>
2019-09-29 18:39:29 -0300

    JogoPlataforma   Implementado as animações de correr, pular e idle.

--- GamePlataforma/Jogador.gd
+++ GamePlataforma/Jogador.gd
@@ -5,6 +5,9 @@ const GRAVIDADE = 10;
 const PULO_FORCA = -250;
 const CHAO = Vector2(0, -1);
 
+const NAO_ESPELHAR = Vector2(1, 1);
+const ESPELHAR = Vector2(-1, 1);
+
 var velocidade = Vector2();
 
 var podePular = false;
@@ -12,10 +15,16 @@ var podePular = false;
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocidade.x = SPEED;
+		$AnimationPlayer.play("run");
+		$Sprite.scale = NAO_ESPELHAR;
 	elif Input.is_action_pressed("ui_left"):
 		velocidade.x = -SPEED;
+		$AnimationPlayer.play("run");
+		$Sprite.scale = ESPELHAR;
 	else:
 		velocidade.x = 0;
+		if is_on_floor():
+			$AnimationPlayer.play("idle");
 		
 	podePular = Input.is_action_pressed("ui_up") and is_on_floor();
 	
@@ -23,5 +32,11 @@ func _physics_process(delta):
 		velocidade.y = PULO_FORCA;
 		
 	velocidade.y += GRAVIDADE;
-		
+	
+	if not(is_on_floor()): 
+		if velocidade.y < 0:
+			$AnimationPlayer.play("jump");
+		else:
+			$AnimationPlayer.play("fall");
+	
 	velocidade = move_and_slide(velocidade, CHAO);
